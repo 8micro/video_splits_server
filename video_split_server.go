9c54505ade256703995f2ac4dd6cb3f9ed0694d1
cdmaji1 <cdmaji1@jd.com>
2018-01-20 19:13:36 +0800

    debug  recv split request and split video to destination dir ok

--- video_split_server.go
+++ video_split_server.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
@@ -9,17 +9,28 @@ import (
 	"os"
 	"os/exec"
 	"strconv"
-
-	uuid "github.com/satori/go.uuid"
 )
 
 var FFMPEGPath string = "/data/web/ffmpeg_ubuntu/ffmpeg-git-20171206-64bit-static/ffmpeg"
 var SplitTimeInterval int32 = 5
 
+type SplitRequest struct {
+	UserID        string `json:"userid"`       // user id
+	UUID          string `json:"uuid"`         //file upload uuid
+	FilePathName  string `json:"filepathname"` //full path name
+	FileDir       string `json:"filedir"`      // file dir
+	FileName      string `json:"filename"`     //file name
+	VideoHeight   string `json:videoheight`
+	VideoWidth    string `json:"videowidth"`
+	VideoRate     string `json:"videorate"`
+	VideoDuration string `json:"videoDuration"`
+	VideoFileSize string `json:"videofilesize"`
+}
+
 func main() {
 	//todo http server
 
-	go doSplitTest()
+	//go doSplitTest()
 
 	http.HandleFunc("/split_video", SplitVideoHandler)
 
@@ -37,49 +48,79 @@ func SplitVideoHandler(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+/*
+	UserID        string `json:"userid"`       // user id
+	UUID          string `json:"uuid"`         //file upload uuid
+	FilePathName  string `json:"filepathname"` //full path name
+	FileDir       string `json:"filedir"`      // file dir
+	FileName      string `json:"filename"`     //file name
+	VideoHeight   string `json:videoheight`
+	VideoWidth    string `json:"videowidth"`
+	VideoRate     string `json:"videorate"`
+	VideoDuration string `json:"videoDuration"`
+	VideoFileSize string `json:"videofilesize"`
+*/
+
 func doSplit(w http.ResponseWriter, req *http.Request) {
-	videoPath := req.PostFormValue("video_path")
-	outputPath := req.PostFormValue("output_path")
-	outputVideoListName := req.PostFormValue("video_list_name")
-	//ffmpeg -i test.mp4 -c copy -map 0 -f segment -segment_list playlist.m3u8 -segment_time 5 ./video_output/output%03d.ts
+	body_bytes := make([]byte, req.ContentLength)
 
-	args := fmt.Sprintf("-i %s -c copy -map 0 -f segment -segment_list %s.m3u8 -segment_time %d %s'%03d'.ts", videoPath, outputVideoListName, SplitTimeInterval, outputPath)
+	_, err0 := req.Body.Read(body_bytes)
 
-	cmd := exec.Command(FFMPEGPath, args)
-	out := new(bytes.Buffer)
-	cmd.Stdout = out
-	err := cmd.Run()
-	if err != nil {
-		log.Printf("run cmd  %s  failed", args)
+	log.Printf("recv split request : %v", body_bytes)
+	var request SplitRequest
+	err0 = json.Unmarshal(body_bytes, &request)
+	if err0 != nil {
+		log.Printf("Unmarshal failed  %v for %v ", err0.Error(), body_bytes)
 		return
 	}
-	outStr := out.String()
-	log.Printf("execute result %s", outStr)
-}
+	log.Printf("split request %v", request)
+	userID := request.UserID
+	uuid := request.UUID
+	filePathName := request.FilePathName
+	// fileDir := request.FilePathName
+	// fileName := request.FileName
+	// videoheight := request.VideoHeight
+	// videoWidth := request.VideoWidth
+	// videoRate := request.VideoRate
+	// videoDuration := request.VideoDuration
+	// videoFileSize := request.VideoFileSize
 
-func doSplitTest() {
-	args := fmt.Sprintf("-i %s -c copy -map 0 -f segment -segment_list /data/web/video/test_output/%s.m3u8 -segment_time %d %s%s03d.ts",
-		"/data/web/video/test2.mp4", "split_test", 5, "/data/web/video/test_output/split_test_", "%")
-	//args := EncodingArgs("/data/web/video/test2.mp4", "/data/web/video/test_output/", "test_video_manifest", 5, "/data/web/video/test_output/", "split_test_")
+	videoManifestFilePathName := filePathName + ".m3u8"
+	videoSegmentsPathNamePrefix := filePathName
 
-	err := execute(FFMPEGPath, []string{args})
-	if err != nil {
-		log.Fatal(err)
+	//outputVideoListName := req.PostFormValue("video_list_name")
+	//ffmpeg -i test.mp4 -c copy -map 0 -f segment -segment_list playlist.m3u8 -segment_time 5 ./video_output/output%03d.ts
+
+	args := fmt.Sprintf(" -i %s -c copy -map 0 -f segment -segment_list %s -segment_time %d %s%s03d.ts",
+		filePathName, videoManifestFilePathName, SplitTimeInterval, videoSegmentsPathNamePrefix, "%")
+
+	err1 := execute(FFMPEGPath, args, userID, uuid)
+	if err1 != nil {
+		log.Printf("execute split script failed for user %s  file %s \n", userID, filePathName)
 	}
 
+	//send post request to api server
+
 }
 
-func execute(cmdPath string, args []string) (err error) {
+//fmt.Sprintf(" -i %s -c copy -map 0 -f segment -segment_list %s -segment_time %d %s%s03d.ts",
+//	"/data/web/video/test2.mp4", "split_test", 5, "/data/web/video/test_output/split_test_", "%")
+//
+func execute(cmdPath string, args string, userId string, uuid string) (err error) {
 
-	u1 := uuid.Must(uuid.NewV4())
-	uuidStr := u1.String()
-	scriptPathName := fmt.Sprintf("/tmp/%s.sh", uuidStr)
+	scriptPathName := fmt.Sprintf("/tmp/%s/%s.sh", userId, uuid)
+	scriptDir := fmt.Sprintf("/tmp/%s/", userId)
+
+	if err0 := os.MkdirAll(scriptDir, 0777); err0 != nil {
+		log.Printf("create dir failed %s", scriptDir)
+		return errors.New("create dir failed %s")
+	}
 
 	log.Printf("create script at %s", scriptPathName)
 
 	f, err := os.Create(scriptPathName)
 	if err != nil {
-		log.Printf("create script file faild")
+		log.Printf("create script file failed %v", err.Error())
 		return errors.New("create script file failed")
 	}
 
@@ -88,9 +129,7 @@ func execute(cmdPath string, args []string) (err error) {
 
 	f.Write(scriptBeginBytes)
 
-	cmdArgs := fmt.Sprintf(" -i %s -c copy -map 0 -f segment -segment_list /data/web/video/test_output/%s.m3u8 -segment_time %d %s%s03d.ts",
-		"/data/web/video/test2.mp4", "split_test", 5, "/data/web/video/test_output/split_test_", "%")
-	cmdStr := cmdPath + cmdArgs
+	cmdStr := cmdPath + args
 
 	cmdBytes := []byte(cmdStr)
 	var writeBytes int
