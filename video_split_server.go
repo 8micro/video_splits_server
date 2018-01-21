56abb4f8b4632d5a8b599009ed474578926a33d9
cdmaji1 <cdmaji1@jd.com>
2018-01-21 15:57:09 +0800

    修改存库的协议

--- video_split_server.go
+++ video_split_server.go
@@ -2,19 +2,24 @@ package main
 
 import (
 	"bytes"
+	"crypto/md5"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
+	"strconv"
+	"strings"
+	"time"
 )
 
 var FFMPEGPath string = "/data/web/ffmpeg_ubuntu/ffmpeg-git-20171206-64bit-static/ffmpeg"
 var SplitTimeInterval int32 = 5
 
-const DbURL string = "http://127.0.0.1/save_video"
+const DbURL string = "http://127.0.0.1:8086/datastore/v1/files"
 
 type SplitRequest struct {
 	UserID        string `json:"userid"`       // user id
@@ -30,17 +35,19 @@ type SplitRequest struct {
 }
 
 type SaveVideoRequest struct {
-	UserID                    string `json:"userid"`
-	UUID                      string `json:"uuid"`
-	VideoOriginFilePathName   string `json:"video_file_path_name"`
-	VideoOriginFileDir        string `json:"video_file_dir"`
-	VideoFileManifestPathName string `json:"video_manifest"` // full path for video splits manifest file .m3u8
-	VideoManifestDir          string `json:"video_manifest_dir"`
-	VideoHeight               string `json:"video_height"`
-	VideoWidth                string `json:"video_width"`
-	VideoRate                 string `json:"video_rate"`
-	VideoDuration             string `json:"video_duration"`
-	VideoFileSize             string `json:"video_file_size"`
+	UserID                    string `json:"UserID"`
+	UUID                      string `json:"UUID"`
+	VideoOriginFilePathName   string `json:"FilePathName"`
+	VideoOriginFileDir        string `json:"VideoFileDir"`
+	VideoFileManifestPathName string `json:"ManifestPathName"` // full path for video splits manifest file .m3u8
+	VideoManifestDir          string `json:"VideoManifestDir"`
+	VideoHeight               string `json:"VideoHeight"`
+	VideoWidth                string `json:"VideoWidth"`
+	VideoRate                 string `json:"VideoRate"`
+	VideoDuration             string `json:"VideoDuration"`
+	VideoFileSize             int64  `json:"FileSize"`
+	VerifyCode                string `json:"VerifyCode"`
+	UploadTimestamp           int64  `json:"UploadAt"`
 }
 
 func main() {
@@ -82,7 +89,6 @@ func doSplit(w http.ResponseWriter, req *http.Request) {
 
 	_, err0 := req.Body.Read(body_bytes)
 
-	log.Printf("recv split request : %v", body_bytes)
 	var request SplitRequest
 	err0 = json.Unmarshal(body_bytes, &request)
 	if err0 != nil {
@@ -97,9 +103,9 @@ func doSplit(w http.ResponseWriter, req *http.Request) {
 	//fileName := request.FileName
 	videoheight := request.VideoHeight
 	videoWidth := request.VideoWidth
-	videoRate := request.VideoRate
-	videoDuration := request.VideoDuration
-	videoFileSize := request.VideoFileSize
+	videoRate := strings.Trim(request.VideoRate, "\"")
+	videoDuration := strings.Trim(request.VideoDuration, "\"")
+	videoFileSize := strings.Trim(request.VideoFileSize, "\"")
 
 	videoManifestFilePathName := filePathName + ".m3u8"
 	videoSegmentsPathNamePrefix := filePathName
@@ -114,36 +120,50 @@ func doSplit(w http.ResponseWriter, req *http.Request) {
 	if err1 != nil {
 		log.Printf("execute split script failed for user %s  file %s \n", userID, filePathName)
 	}
-
+	verifyCode, err4 := ComputeMd5(filePathName)
+	if err4 != nil {
+		log.Printf("compute md5 failed")
+	}
 	//send post request to api server
+	ivideoFileSize, err3 := strconv.ParseInt(videoFileSize, 10, 64)
+	if err3 != nil {
+		log.Printf("convert filesize to int64 failed %v\n", ivideoFileSize)
+		return
+	}
 	var saveRequest SaveVideoRequest
 	saveRequest.UserID = userID
 	saveRequest.UUID = uuid
 	saveRequest.VideoDuration = videoDuration
 	saveRequest.VideoFileManifestPathName = videoManifestFilePathName
-	saveRequest.VideoFileSize = videoFileSize
+	saveRequest.VideoFileSize = ivideoFileSize
 	saveRequest.VideoHeight = videoheight
 	saveRequest.VideoManifestDir = fileDir
 	saveRequest.VideoOriginFileDir = fileDir
 	saveRequest.VideoOriginFilePathName = filePathName
 	saveRequest.VideoRate = videoRate
 	saveRequest.VideoWidth = videoWidth
+	saveRequest.VerifyCode = verifyCode
+	saveRequest.UploadTimestamp = time.Now().Unix()
 
 	b, err2 := json.Marshal(saveRequest)
 	if err2 != nil {
 		log.Printf("marshal json save request failed for %v", saveRequest)
 		return
 	}
+	log.Printf("save request [%v]", saveRequest)
 	sendreq := bytes.NewBuffer(b)
 	resp, err3 := http.Post(DbURL, "application/json;charset=utf-8", sendreq)
 	if err3 != nil {
-		log.Printf("send save request to db failed")
+		log.Printf("send save request to db failed, %v", err3.Error())
 		return
 	}
+	defer resp.Body.Close()
 	if resp.Status == "200 OK" {
-		log.Printf("save video to db success")
+		resp_bytes := make([]byte, resp.ContentLength)
+		resp.Body.Read(resp_bytes)
+		log.Printf("save video to db success, response %v", string(resp_bytes[:]))
 	} else {
-		log.Printf("save video to db failed %v", saveRequest)
+		log.Printf("save video to db failed status %v \n [%v] \n", resp.Status, string(b[:]))
 	}
 
 }
@@ -200,4 +220,22 @@ func execute(cmdPath string, args string, userId string, uuid string) (err error
 	log.Printf("execute finished")
 
 	return nil
+}
+
+func ComputeMd5(filePathName string) (string, error) {
+	f, err := os.Open(filePathName)
+	if err != nil {
+		log.Fatal(err)
+		return "", errors.New("open file failed for md5")
+	}
+	defer f.Close()
+
+	h := md5.New()
+	if _, err := io.Copy(h, f); err != nil {
+		log.Fatal(err)
+		return "", errors.New("copy file failed for md5")
+	}
+
+	md5 := fmt.Sprintf("%x", h.Sum(nil))
+	return md5, nil
 }
